1ceeb9db1888738947d8c7baf55eeb11263ac6a3
cold-snowflake <cold.snowflake01@gmail.com>
2024-02-10 14:32:35 +0100

    game two hogwarc

--- app/main.py
+++ app/main.py
@@ -1,27 +1,28 @@
 from flask import Flask, render_template, request
-from utils import get_answer, answers
+from utils import get_answer, answers, houses
 
 app = Flask(__name__)
 
 @app.route('/')
 def show_user_profile():
-    return render_template("main.html")
+    return render_template("game_one/main.html")
 
 
 @app.route('/game_one', methods=['GET', 'POST'])
 def game_one():
     if request.method == 'GET':
-        return render_template('game_one.html')
-    return render_template("answer.html", name = request.form.get('name').capitalize(),
+        return render_template('game_one/game_one.html')
+    return render_template("game_one/answer.html", name = request.form.get('name').capitalize(),
                             answer = get_answer(answers))
 
-# @app.route('/path/<path:subpath>')
-# def show_subpath(subpath):
-#     # show the subpath after /path/
-#     return f'Subpath {escape(subpath)}'
 
-# with app.test_request_context():
-#     print(url_for('show_user_profile'))
-#     print(url_for('show_post'))
-    # print(url_for('login', next='/'))
-    # print(url_for('profile', username='John Doe'))
+@app.route('/game_two', methods= ['GET', 'POST'])
+def game_two_enter_name():
+    if request.method == 'GET':
+        return render_template("game_two/game_two_enter.html")
+    return render_template("game_two/quetion_one.html")
+
+@app.route('/game_two/result')
+def result_game_two():
+    return render_template("game_two/result.html",result = get_answer(houses))
+    
